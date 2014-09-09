247202a1089ca36d4e3add5218b9e628022b18c3
Carlos Lozano <carlos@adaptive.me>
2014-09-09 21:56:12 +0200

    iOS/OSX Work in Progress.

---
+++ adaptive-arp-impl-universal/AppContextImpl.swift
@@ -0,0 +1,9 @@
+//
+//  AppContextImpl.swift
+//  AdaptiveArpImplIos
+//
+//  Created by Carlos Lozano Diez on 09/09/2014.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+
+import Foundation
