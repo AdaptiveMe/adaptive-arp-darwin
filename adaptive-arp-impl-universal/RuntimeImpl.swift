df932c86179d86b5b1ba1248982a0e06eeea0d1e
Ferran Vilà Conesa <ferran.vila.conesa@gmail.com>
2014-09-10 15:50:50 +0200

    Comments on classes
    TODO: implementation of this clases

--- adaptive-arp-impl-universal/RuntimeImpl.swift
+++ adaptive-arp-impl-universal/RuntimeImpl.swift
@@ -21,7 +21,10 @@
 *
 * Contributors:
 *
-*     *
+*     * Ferran Vila Conesa
+*                 <http://github.com/fnva>
+*                 <http://twitter.com/ferran_vila>
+*                 <mailto:ferran.vila.conesa@gmail.com>
 *
 * =====================================================================================================================
 */
@@ -30,15 +33,25 @@ import Foundation
 
 public class RuntimeImpl : IRuntime {
     
+    /// Logging variable
+    let logger : ILogging = LoggingImpl()
+    
+    /**
+    Class constructor
+    */
     init() {
         
     }
     
     public func dismissApplication() {
         
+        // TODO
     }
     
     public func dismissSplashScreen() -> Bool {
+        
+        // TODO
+        
         return false
     }
     
