15c185e4f57133e719c8efd47201d77eb7ceea39
Ferran Vilà Conesa <ferran.vila.conesa@gmail.com>
2014-09-15 08:40:07 +0200

    Utils Initial Implementation

---
+++ adaptive-arp-impl-universal/utils/Utils.swift
@@ -0,0 +1,55 @@
+/*
+* =| ADAPTIVE RUNTIME PLATFORM |=======================================================================================
+*
+* (C) Copyright 2013-2014 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+* an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*
+* Original author:
+*
+*     * Carlos Lozano Diez
+*                 <http://github.com/carloslozano>
+*                 <http://twitter.com/adaptivecoder>
+*                 <mailto:carlos@adaptive.me>
+*
+* Contributors:
+*
+*     * Ferran Vila Conesa
+*                 <http://github.com/fnva>
+*                 <http://twitter.com/ferran_vila>
+*                 <mailto:ferran.vila.conesa@gmail.com>
+*
+* =====================================================================================================================
+*/
+
+import Foundation
+
+public struct Utils {
+    
+    /**
+    Function that checks if an url has the correct sintaxis
+    
+    :param: stringURL url to check
+    
+    :returns: true if correct, false otherwise
+    :author: Ferran Vila Conesa
+    :since: ARP1.0
+    */
+    public static func validateUrl (stringURL : NSString) -> Bool {
+        
+        var urlRegEx = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", argumentArray:[urlRegEx])
+        var urlTest = NSPredicate.predicateWithSubstitutionVariables(predicate)
+        
+        return predicate.evaluateWithObject(stringURL)
+    }
+}
+
+
