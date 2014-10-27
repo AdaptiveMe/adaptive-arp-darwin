0b2423846772780ce490be8497d7058291c296a7
Ferran Vilà Conesa <ferran.vila.conesa@gmail.com>
2014-10-27 08:42:46 +0100

    Logging and OS Test

---
+++ adaptive-arp-rt-ios/AdaptiveArpRtIosTests/LoggingTest.swift
@@ -0,0 +1,64 @@
+/*
+* =| ADAPTIVE RUNTIME PLATFORM |=======================================================================================
+*
+* (C) Copyright 2013-2014 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+* an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*
+* Original author:
+*
+*     * Carlos Lozano Diez
+*                 <http://github.com/carloslozano>
+*                 <http://twitter.com/adaptivecoder>
+*                 <mailto:carlos@adaptive.me>
+*
+* Contributors:
+*
+*     * Ferran Vila Conesa
+*                 <http://github.com/fnva>
+*                 <http://twitter.com/ferran_vila>
+*                 <mailto:ferran.vila.conesa@gmail.com>
+*
+* =====================================================================================================================
+*/
+
+import UIKit
+import XCTest
+
+class LoggingTest: XCTestCase {
+    
+    var loggingImpl:LoggingImpl?
+    
+    override func setUp() {
+        super.setUp()
+        
+        loggingImpl = LoggingImpl()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testGetOSInfo() {
+        
+        XCTAssert(loggingImpl?.log(ILoggingLogLevel.DEBUG, message: "Logging test") != nil, "")
+        XCTAssert(loggingImpl?.log(ILoggingLogLevel.DEBUG, category: "Logging Category", message: "Logging test") != nil, "")
+    }
+    
+    func testPerformanceGetOSInfo() {
+        
+        self.measureBlock() {
+            
+            self.loggingImpl?.log(ILoggingLogLevel.DEBUG, message: "Logging test")
+            self.loggingImpl?.log(ILoggingLogLevel.DEBUG, category: "Logging Category", message: "Logging test")
+        }
+    }
+    
+}
