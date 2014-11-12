82048d9ba420180d4cac4b3b827104a74331b72a
Carlos Lozano <carlos@adaptive.me>
2014-11-12 11:27:00 +0100

    Adaptive API updated.

---
+++ adaptive-arp-impl/Source/Common/AppResourceManager.swift
@@ -0,0 +1,9 @@
+//
+//  AppResourceManager.swift
+//  adaptive-arp-impl
+//
+//  Created by Carlos Lozano Diez on 12/11/14.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+
+import Foundation
