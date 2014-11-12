bb82325616332c0add1d510f079d89c385baffab
Carlos Lozano <carlos@adaptive.me>
2014-11-12 12:40:19 +0100

    Alamofire sub-module updated.

--- adaptive-arp-impl/Source/Common/AppResourceManager.swift
+++ adaptive-arp-impl/Source/Common/AppResourceManager.swift
@@ -1,9 +1,48 @@
-//
-//  AppResourceManager.swift
-//  adaptive-arp-impl
-//
-//  Created by Carlos Lozano Diez on 12/11/14.
-//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
-//
+/*
+* =| ADAPTIVE RUNTIME PLATFORM |=======================================================================================
+*
+* (C) Copyright 2013-2014 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+* an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
+*
+* Original author:
+*
+*     * Carlos Lozano Diez
+*                 <http://github.com/carloslozano>
+*                 <http://twitter.com/adaptivecoder>
+*                 <mailto:carlos@adaptive.me>
+*
+* Contributors:
+*
+*     *
+*
+* =====================================================================================================================
+*/
 
-import Foundation
+#if os(iOS)
+    import Realm
+#elseif os(OSX)
+    import Realm
+#endif
+
+public class AppResourceManager {
+    
+    /// Singleton instance
+    public class var sharedInstance : AppResourceManager {
+        struct Static {
+            static let instance : AppResourceManager = AppResourceManager()
+        }
+        return Static.instance
+    }
+    
+    init() {
+        
+    }
+}
