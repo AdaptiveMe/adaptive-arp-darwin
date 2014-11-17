b6a4e667138b0658a75ef50a922b28ce3e53b66d
Carlos Lozano <carlos@adaptive.me>
2014-11-17 19:52:05 +0100

    Fixes #49.

--- adaptive-arp-impl/update_thirdparty.sh
+++ adaptive-arp-impl/update_thirdparty.sh
@@ -26,7 +26,7 @@
 #
 # =====================================================================================================================
 
-array=( Alamofire CryptoSwift realm-cocoa SQLite.swift SwiftyJSON )
+array=( Alamofire realm-cocoa SQLite.swift SwiftyJSON )
 for project in "${array[@]}"
 do
     echo Processing third-party module $project .
