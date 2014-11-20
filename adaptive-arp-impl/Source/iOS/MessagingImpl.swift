14abbccea9fa5461da222d83a40a6d0bd15b7276
Ferran Vilà Conesa <ferran.vila.conesa@gmail.com>
2014-11-20 17:38:59 +0100

    Messaging Implementation

--- adaptive-arp-impl/Source/iOS/MessagingImpl.swift
+++ adaptive-arp-impl/Source/iOS/MessagingImpl.swift
@@ -31,194 +31,226 @@
 
 import Foundation
 import MessageUI
-import UIKit
-import WebKit
 import AdaptiveArpApi
 
 /**
 This class implements also the MFMessageComposeViewControllerDelegate in order to handle the callbacks from a sms call and the MFMailComposeViewControllerDelegate to handle mail callbacks
 :see: https://github.com/hectorlr/Reddit-Scan-Swift/blob/master/Reddit%20Scan%20Swift/Views/ShareCard.swift
 */
-public class MessagingImpl : NSObject, IMessaging, MFMessageComposeViewControllerDelegate, MFMailComposeViewControllerDelegate {
+public class MessagingImpl :  UIViewController, IMessaging, MFMessageComposeViewControllerDelegate, MFMailComposeViewControllerDelegate {
     
     /// Logging variable
     let logger : ILogging = LoggingImpl()
+    let loggerTag : String = "MessagingImpl"
     
-    var application:UIApplication!
-    var webView:UIView!
+    /// Callbacks
+    var smsCallback:IMessagingCallback!
+    var mailCallback:IMessagingCallback!
     
-    /// Callback for returning the sms responses
-    var smsCallback: IMessagingCallback?
-    
-    /// Callback for returning the mail responses
-    var mailCallback: IMessagingCallback?
-    
-    /**
-    Class constructor
-    */
+    /// Class constructor
     public override init() {
-        application = (AppRegistryImpl.sharedInstance.getPlatformContext()! as AppContextImpl).getContext() as UIApplication
-        webView = (AppRegistryImpl.sharedInstance.getPlatformContextWeb()! as AppContextWebviewImpl).getWebviewPrimary() as UIView
+        super.init()
     }
     
-    /**
-    Send an Email
-    
-    :param: data     the email data
-    :param: callback with the result
-    :author: Ferran Vila Conesa
-    :since: ARP1.0
-    */
+    /// Send an Email
+    /// :param: data     the email data
+    /// :param: callback with the result
+    /// :author: Ferran Vila Conesa
+    /// :since: ARP1.0
     public func sendEmail(data : Email, callback : IMessagingCallback) {
-        
-        // TODO: warns and errors not handled: Unable_To_Sent_All, Unable_to_fetch_attachment, Email_Account_Not_Found, SIM_Not_Present, Wrong_Params
         
         // Check if the device can send Mails
         if !MFMailComposeViewController.canSendMail() {
             
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "The device cannot send a mail. Check the device")
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "The device cannot send a mail. Check the device")
             
             callback.onError(IMessagingCallbackError.NotSupported)
             return
-        }
-        
-        self.mailCallback = callback
-        
-        // Create the email with the fields
-        var mail: MFMailComposeViewController = MFMailComposeViewController()
-        mail.mailComposeDelegate = self
-        
-        // addresses (to)
-        var recipientsTo: [String] = [String]()
-        for mail:EmailAddress in data.getToRecipients()!{
-            recipientsTo.append(mail.getAddress()!)
-        }
-        mail.setToRecipients(recipientsTo)
-        
-        // addresses (cc)
-        var recipientsCc: [String] = [String]()
-        for mail:EmailAddress in data.getCcRecipients()!{
-            recipientsCc.append(mail.getAddress()!)
-        }
-        mail.setToRecipients(recipientsCc)
-        
-        // addresses (bcc)
-        var recipientsBcc: [String] = [String]()
-        for mail:EmailAddress in data.getBccRecipients()!{
-            recipientsBcc.append(mail.getAddress()!)
-        }
-        mail.setToRecipients(recipientsBcc)
-        
-        // TODO: is not using the messageBodyMimeType
-        
-        mail.setSubject(data.getSubject())
-        mail.setMessageBody(data.getMessageBody(), isHTML: true)
-        
-        // atachments
-        
-        for attachment: AttachmentData in data.getAttachmentData()! {
-            
-            var nsData:NSData = NSData(bytes: attachment.getData()! as [Byte], length: Int(attachment.getDataSize()))
-            
-            mail.addAttachmentData(nsData, mimeType: attachment.getMimeType(), fileName: attachment.getFileName())
-            
         }
         
         // Open the view to compose the mail with the fields setted
-        webView.window?.rootViewController?.presentViewController(mail, animated: true, completion: nil)
+        if let view = BaseViewController.ViewCurrent.getView() {
+                
+            self.mailCallback = callback
+            
+            // Add the MessageImplementation view controller and the subview
+            view.addChildViewController(self)
+            view.view.addSubview(self.view)
+                
+            // Create the email with the fields
+            var mail: MFMailComposeViewController = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            
+            // addresses (to)
+            var recipientsTo: [String] = [String]()
+            for mail:EmailAddress in data.getToRecipients()!{
+                recipientsTo.append(mail.getAddress()!)
+            }
+            mail.setToRecipients(recipientsTo)
+            
+            // addresses (cc)
+            var recipientsCc: [String] = [String]()
+            for mail:EmailAddress in data.getCcRecipients()!{
+                recipientsCc.append(mail.getAddress()!)
+            }
+            mail.setCcRecipients(recipientsCc)
+            
+            // addresses (bcc)
+            var recipientsBcc: [String] = [String]()
+            for mail:EmailAddress in data.getBccRecipients()!{
+                recipientsBcc.append(mail.getAddress()!)
+            }
+            mail.setBccRecipients(recipientsBcc)
+            
+            mail.setSubject(data.getSubject())
+            mail.setMessageBody(data.getMessageBody(), isHTML: true)
+            
+            // atachments
+            
+            for attachment: AttachmentData in data.getAttachmentData()! {
+                
+                var nsData:NSData = NSData(bytes: attachment.getData()! as [Byte], length: Int(attachment.getDataSize()))
+                
+                mail.addAttachmentData(nsData, mimeType: attachment.getMimeType(), fileName: attachment.getFileName())
+                
+            }
+            
+            self.presentViewController(mail, animated: true, completion: {
+                self.logger.log(ILoggingLogLevel.DEBUG, category: self.loggerTag, message: "MFMailComposeViewController presented")
+            })
+            
+        } else {
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "There is no a current view controller on the stack")
+            callback.onError(IMessagingCallbackError.NotSent)
+        }
     }
     
-    /**
-    Send text SMS
-    
-    :param: number   to send
-    :param: text     to send
-    :param: callback with the result
-    :author: Ferran Vila Conesa
-    :since: ARP1.0
-    */
+    /// Send text SMS
+    /// :param: number   to send
+    /// :param: text     to send
+    /// :param: callback with the result
+    /// :author: Ferran Vila Conesa
+    /// :since: ARP1.0
     public func sendSMS(number : String, text : String, callback : IMessagingCallback) {
         
-        // TODO: warns and errors not handled: SIM_Not_Present, Wrong_Params
-        
         // Check if the device can send SMS
-        if !MFMessageComposeViewController.canSendText() {
+        if !(MFMessageComposeViewController.canSendText()) {
             
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "The device cannot send SMS. Check the device")
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "The device cannot send SMS. Check the device")
             
             callback.onError(IMessagingCallbackError.NotSupported)
             return
         }
         
-        self.smsCallback = callback
-
-        // Create the message and set the fields
-        var messageController: MFMessageComposeViewController = MFMessageComposeViewController(nibName: nil, bundle: nil)
-        messageController.messageComposeDelegate = self
-        messageController.body = text
-        messageController.recipients = [number]
-        
-        // Open the view to compose the message with the fields setted
-        webView.window?.rootViewController?.presentViewController(messageController, animated: true, completion: nil)
+        // Open the view to compose the mail with the fields setted
+        if let view = BaseViewController.ViewCurrent.getView() {
+            
+            self.smsCallback = callback
+            
+            // Add the MessageImplementation view controller and the subview
+            view.addChildViewController(self)
+            view.view.addSubview(self.view)
+            
+            let sms: MFMessageComposeViewController = MFMessageComposeViewController()
+            sms.messageComposeDelegate = self
+            sms.body = text
+            sms.recipients = [number]
+            
+            self.presentViewController(sms, animated: true, completion: {
+                self.logger.log(ILoggingLogLevel.DEBUG, category: self.loggerTag, message: "MFMessageComposeViewController presented")
+            })
+            
+        } else {
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "There is no a current view controller on the stack")
+            callback.onError(IMessagingCallbackError.NotSent)
+        }
     }
     
-    /**
-    This method is the metod called by the OS when the user leaves the smss screen. This method receives the result of the opertaion
-    
-    :param: sendMsg MFMessageComposeViewController used to fire this event
-    :param: result  Send SMS result
-    */
-    public func messageComposeViewController(sendMsg: MFMessageComposeViewController, didFinishWithResult result: MessageComposeResult) {
+    /// This method is the metod called by the OS when the user leaves the smss screen. This method receives the result of the opertaion
+    /// :param: controller MFMessageComposeViewController used to fire this event
+    /// :param: result  Send SMS result
+    /// :author: Ferran Vila Conesa
+    /// :since: ARP1.0
+    public func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
         
         switch result.value {
             
         case MessageComposeResultSent.value :
-            logger.log(ILoggingLogLevel.DEBUG, category: "MessagingImpl", message: "Message sent")
-            smsCallback!.onResult(true)
-            
+            logger.log(ILoggingLogLevel.DEBUG, category: loggerTag, message: "Message sent")
+            smsCallback.onResult(true)
         case MessageComposeResultCancelled.value :
-            logger.log(ILoggingLogLevel.DEBUG, category: "MessagingImpl", message: "Message cancelled by the user")
-            smsCallback!.onResult(false)
-            
+            logger.log(ILoggingLogLevel.DEBUG, category: loggerTag, message: "Message cancelled by the user")
+            smsCallback.onResult(false)
         case MessageComposeResultFailed.value :
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "Message not send due to an error")
-            smsCallback!.onError(IMessagingCallbackError.NotSent)
-            
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "Message not send due to an error")
+            smsCallback.onError(IMessagingCallbackError.NotSent)
         default:
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "The message delegeate received an unsuported result")
-            
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "The message delegeate received an unsuported result")
         }
         
-        //parent.dismissViewControllerAnimated(true, completion:nil)
-        
+        // Remove the view
+        if let view = BaseViewController.ViewCurrent.getView() {
+            
+            // Remove the Message, Remove the view and remove the parent view controller
+            self.dismissViewControllerAnimated(true, completion: nil)
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        } else {
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "There is no a current view controller on the stack")
+            smsCallback.onError(IMessagingCallbackError.Unknown)
+        }
     }
     
-    public func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!){
+    /// This method is the metod called by the OS when the user leaves the mail screen. This method receives the result of the opertaion
+    /// :param: sendMsg MFMailComposeViewController used to fire this event
+    /// :param: result  Send Mail result
+    /// :author: Ferran Vila Conesa
+    /// :since: ARP1.0
+    public func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError!){
         
         switch result.value {
             
         case MFMailComposeResultCancelled.value:
-            logger.log(ILoggingLogLevel.DEBUG, category: "MessagingImpl", message: "Mail cancelled by the user")
+            logger.log(ILoggingLogLevel.DEBUG, category: loggerTag, message: "Mail cancelled by the user")
             mailCallback!.onResult(false)
-            
         case MFMailComposeResultSaved.value:
-            logger.log(ILoggingLogLevel.DEBUG, category: "MessagingImpl", message: "Mail saved")
-            mailCallback!.onResult(true)
-            
+            logger.log(ILoggingLogLevel.DEBUG, category: loggerTag, message: "Mail saved")
+            mailCallback.onResult(false)
         case MFMailComposeResultSent.value:
-            logger.log(ILoggingLogLevel.DEBUG, category: "MessagingImpl", message: "Mail sent")
-            mailCallback!.onResult(true)
-            
+            logger.log(ILoggingLogLevel.DEBUG, category: loggerTag, message: "Mail sent")
+            mailCallback.onResult(true)
         case MFMailComposeResultFailed.value:
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "Mail not send due to an error: \(error.localizedDescription)")
-            mailCallback!.onError(IMessagingCallbackError.NotSent)
-            
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "Mail not send due to an error: \(error.localizedDescription)")
+            mailCallback.onError(IMessagingCallbackError.NotSent)
         default:
-            logger.log(ILoggingLogLevel.ERROR, category: "MessagingImpl", message: "The mail delegeate received an unsuported result")
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "The mail delegeate received an unsuported result")
         }
         
-        // Close the Mail Interface
-        // self.parent.dismissViewControllerAnimated(true, completion:nil)
+        // Remove the view
+        if let view = BaseViewController.ViewCurrent.getView() {
+            
+            // Remove the Message, Remove the view and remove the parent view controller
+            self.dismissViewControllerAnimated(true, completion: nil)
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        } else {
+            logger.log(ILoggingLogLevel.ERROR, category: loggerTag, message: "There is no a current view controller on the stack")
+            mailCallback.onError(IMessagingCallbackError.Unknown)
+        }
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    public override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Set the background color to transparent
+        self.view.backgroundColor = UIColor.clearColor()
     }
     
 }
