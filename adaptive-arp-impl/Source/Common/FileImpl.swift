e641888af27c3452cc72c79d024fc324c90e6832
Ferran Vilà Conesa <ferran.vila.conesa@gmail.com>
2014-11-24 08:37:57 +0100

    Adaptive API updated.

---
+++ adaptive-arp-impl/Source/Common/FileImpl.swift
@@ -0,0 +1,9 @@
+//
+//  FileImpl.swift
+//  adaptive-arp-impl
+//
+//  Created by Administrator on 21/11/14.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+
+import Foundation
